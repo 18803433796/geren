05cff90c9411fd3d58dfbddc06d3d66266b5d6e6
18803433796 <1912844827@qq.com>
2017-01-14 17:48:23 +0800

    add

---
+++ js/page.js
@@ -0,0 +1,14 @@
+var page_content = [
+'<div class="default"><img alt="" src="http://cn.yimg.com/xx1/2012032317.jpg" height="441" width="277"></a><p align="center"></a></div>',
+'<div class="default"><img src="http://cn.yimg.com/2012323huofa/taojiefm2.jpg" style="cursor:pointer;" onclick="$(\'#book .arrow .ar\').click();"/></div>',
+
+
+
+'<div style="font-size:18px;line-height:30px;  text-indent:2em;margin-top:20px"> 我叫宋寿茹,来自山西大同,想要应聘的职位是前端工程师,我是一个对工作特别认真和吃苦的人,性格也比较开朗,可以和别人很好的进行沟通,这是我的优点.人无完人,我也是有缺点的,我的缺点是爱睡懒觉,但是这只是对放假来说的,在工作中绝对不会出现的.</div><div class="xueli" style="font-size: 18px;margin-top:20px">学历:本科</div><div class="zhuanye" style="font-size: 18px;margin-top:15px">专业:计算机科学与技术</div><div class="lian" style="font-size: 18px;margin-top:15px">联系方式:18803433796</div><div class="home" style="font-size: 18px;margin-top:15px">现住址:太原小店区</div><div class="qq" style="font-size:18px;margin-top:18px">qq:1912844827</div>',
+
+
+//最后一页
+'<div style="background:url(images/meinv.png)no-repeat;background-size:cover;height:441px;width:300px;"></div>'];
+
+
+var start_page = 0;
